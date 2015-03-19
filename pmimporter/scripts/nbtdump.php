d0808397a507df9ef16a8b805db086a662ab2aaf
Alejandro Liu <alejandro_liu@hotmail.com>
2015-03-19 15:44:51 +0100

    Added support for old-skool Entities.dat

--- pmimporter/scripts/nbtdump.php
+++ pmimporter/scripts/nbtdump.php
@@ -16,8 +16,15 @@ $dat = file_get_contents($file);
 if ((Binary::readLInt(substr($dat,0,4)) == 2
      || Binary::readLInt(substr($dat,0,4)) == 3)
     && Binary::readLInt(substr($dat,4,4)) == (strlen($dat) - 8)) {
+  // MCPE v0.2.0 level.dat
   $nbt = new NBT(NBT::LITTLE_ENDIAN);
   $nbt->read(substr($dat,8));
+} elseif (substr($dat,0,3) == "ENT" &&  ord(substr($dat,3,1)) == 0 &&
+	  Binary::readLInt(substr($dat,4,4)) == 1 &&
+	  Binary::readLInt(substr($dat,8,4)) == (strlen($dat) - 12)) {
+  // MCPE v0.2.0 entities.dat
+  $nbt = new NBT(NBT::LITTLE_ENDIAN);
+  $nbt->read(substr($dat,12));
 } else {
   $nbt = new NBT(NBT::BIG_ENDIAN);
   $nbt->readCompressed($dat);
