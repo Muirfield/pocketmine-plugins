07ea1095d50a7ac582b5d74c51ad63e85d3c4ac4
Alejandro Liu <alejandro_liu@hotmail.com>
2015-03-19 12:47:19 +0100

    PMF: Added support for Tiles also fixed a HeightMap calculation bug

--- pmimporter/classlib/pmimporter/pm13/Chunk.php
+++ pmimporter/classlib/pmimporter/pm13/Chunk.php
@@ -4,6 +4,13 @@ use pocketmine_1_3\pmf\PMFLevel;
 
 use pocketmine\utils\Binary;
 use pmimporter\ImporterException;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\Compound;
+use pocketmine\nbt\tag\Enum;
+use pocketmine\nbt\tag\Int;
+use pocketmine\nbt\tag\Byte;
+use pocketmine\nbt\tag\String;
+use pocketmine\nbt\tag\Short;
 
 class Chunk implements \pmimporter\Chunk {
   protected $x;
@@ -39,13 +46,13 @@ class Chunk implements \pmimporter\Chunk {
     throw new ImporterException("Unimplemented ".__CLASS__."::".__METHOD__);
   }
   protected function getFloor($x,$z) {
-    for ($y=127;$y<=0;--$y) {
-      if ($this->chuncks->getBlockID($x,$y,$z)) break;
+    for ($y=127;$y>0;--$y) {
+      if ($this->chunks->getBlockID($x,$y,$z)) break;
     }
     return $y;
   }
   public function getHeightMap($x, $z) {
-    $this->getFloor($this->getXPos($x),$this->getZPos($z));
+    return $this->getFloor($this->getXPos($x),$this->getZPos($z));
   }
   public function setHeightMap($x, $z, $value) {
     throw new ImporterException("Unimplemented ".__CLASS__."::".__METHOD__);
@@ -64,7 +71,75 @@ class Chunk implements \pmimporter\Chunk {
   public function setEntities(array $entities = []) {
     throw new ImporterException("Unimplemented ".__CLASS__."::".__METHOD__);
   }
-  public function getTileEntities() { return []; }
+  protected static function convertInventory($name,$src) {
+    $items = [];
+    if (isset($src)) {
+      foreach ($src as $sl) {
+	$items[] = new Compound(false,[new Byte("Count",$sl["Count"]),
+				       new Byte("Slot",$sl["Slot"]),
+				       new Short("id",$sl["id"]),
+				       new Short("Damage",$sl["Damage"])]);
+      }
+    }
+    $nbt = new Enum($name,$items);
+    $nbt->setTagType(NBT::TAG_Compound);
+    return $nbt;
+  }
+  public function getTileEntities() {
+    $tiles = [];
+    $yml = dirname($this->chunks->getFile())."/tiles.yml";
+    if (!file_exists($yml)) return $tiles;
+    $yml = yaml_parse_file($yml);
+    $min_x = $this->getXPos(0); $min_z = $this->getZPos(0);
+    $max_x = $this->getXPos(15); $max_z = $this->getZPos(15);
+    foreach ($yml as $tile) {
+      if (!isset($tile["id"])) continue;
+      if (isset($tile["x"]) && isset($tile["y"]) && isset($tile["z"])) {
+	if ($tile["x"] < $min_x || $tile["x"] > $max_x ||
+	    $tile["z"] < $min_z || $tile["z"] > $max_z) continue;
+      } else {
+	continue;
+      }
+      switch ($tile["id"]) {
+      case TID_SIGN:
+	$tiles[] =  new Compound("",[new String("id",TID_SIGN),
+				     new String("Text1",$tile["Text1"]),
+				     new String("Text2",$tile["Text2"]),
+				     new String("Text3",$tile["Text3"]),
+				     new String("Text4",$tile["Text4"]),
+				     new Int("x",$tile["x"]),
+				     new Int("y",$tile["y"]),
+				     new Int("z",$tile["z"])]);
+	break;
+      case TID_FURNACE:
+	$tiles[] = new Compound("",[new String("id",TID_FURNACE),
+				    new Short("BurnTime",$tile["BurnTime"]),
+				    new Short("BurnTicks",$tile["BurnTicks"]),
+				    new Short("CookTime",$tile["CookTime"]),
+				    new Short("CookTimeTotal",$tile["MaxTime"]),
+				    self::convertInventory("Items",$tile["Items"]),
+				    new Int("x",$tile["x"]),
+				    new Int("y",$tile["y"]),
+				    new Int("z",$tile["z"])]);
+	break;
+      case TID_CHEST:
+	$chest = [new String("id",TID_CHEST),
+		  self::convertInventory("Items",$tile["Items"]),
+		  new Int("x",$tile["x"]),
+		  new Int("y",$tile["y"]),
+		  new Int("z",$tile["z"])];
+	foreach (["pairx","pairz"] as $idx) {
+	  if (isset($tile[$idx])) $chest[] = new Int($idx,$tile[$idx]);
+	}
+	$tiles[] = new Compound("",$chest);
+	break;
+      default:
+	// Not supported tile Id
+	continue;
+      }
+    }
+    return $tiles;
+  }
   public function setTileEntities(array $tiles = []) {
     throw new ImporterException("Unimplemented ".__CLASS__."::".__METHOD__);
   }
