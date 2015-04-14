f0335f6837ff7c97f301e587ceaae71fa5eb7a9c
Alejandro Liu <alejandro_liu@hotmail.com>
2015-04-14 11:28:52 +0200

    Honour cancelled events

--- WorldProtect/src/aliuly/worldprotect/WpPvpMgr.php
+++ WorldProtect/src/aliuly/worldprotect/WpPvpMgr.php
@@ -11,16 +11,17 @@ use pocketmine\Player;
 
 
 class WpPvpMgr implements Listener {
-  public $owner;
-  public function __construct(Plugin $plugin) {
-    $this->owner = $plugin;
-    $this->owner->getServer()->getPluginManager()->registerEvents($this, $this->owner);
-  }
-  public function onPvP(EntityDamageEvent $ev) {
-    if(!($ev instanceof EntityDamageByEntityEvent)) return;
-    if (!($ev->getEntity() instanceof Player && $ev->getDamager() instanceof Player)) return;
-    if ($this->owner->checkPvP($ev->getEntity()->getLevel()->getName())) return;
-    $this->owner->msg($ev->getDamager(),"You are not allowed to do that here");
-    $ev->setCancelled();
-  }
+	public $owner;
+	public function __construct(Plugin $plugin) {
+		$this->owner = $plugin;
+		$this->owner->getServer()->getPluginManager()->registerEvents($this, $this->owner);
+	}
+	public function onPvP(EntityDamageEvent $ev) {
+		if ($ev->isCancelled()) return;
+		if(!($ev instanceof EntityDamageByEntityEvent)) return;
+		if (!($ev->getEntity() instanceof Player && $ev->getDamager() instanceof Player)) return;
+		if ($this->owner->checkPvP($ev->getEntity()->getLevel()->getName())) return;
+		$this->owner->msg($ev->getDamager(),"You are not allowed to do that here");
+		$ev->setCancelled();
+	}
 }
