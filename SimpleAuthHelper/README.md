cb4a0b8d7ff06b44bcc0db1be7484ba85fbd490d
Alejandro Liu <alejandro_liu@hotmail.com>
2015-05-21 07:12:31 +0200

    SimpleAuthHelper-1.2upd2

---
+++ SimpleAuthHelper/README.md
@@ -0,0 +1,121 @@
+<img src="https://raw.githubusercontent.com/alejandroliu/bad-plugins/master/Media/helper.alt-icon.png" style="width:64px;height:64px" width="64" height="64"/>
+
+SimpleAuthHelper
+================
+
+* Summary: Simplifies the SimpleAuth login process
+* Dependency Plugins: n/a
+* PocketMine-MP version: 1.4 - API 1.10.0
+* DependencyPlugins: SimpleAuth
+* OptionalPlugins: -
+* Categories: General
+* Plugin Access: Commands
+* WebSite: [github](https://github.com/alejandroliu/bad-plugins/tree/master/SimpleAuthHelper)
+
+Overview
+--------
+
+Very simple plugin that simplifies the login process... Instead of
+asking for commands, users simply chat away...
+
+### Register process
+
+Player connects for the first time.  They are prompted to enter a
+*NEW* password.  They enter their password directly, without having to
+enter */register*.
+
+They are asked for the password again to confirm.  They re-enter their
+password (again without */register*).
+
+### Login process
+
+Player connects again.  They are prompted to enter their login
+password.  They type their login password directly (without
+*/login*).  And they are in.
+
+Documentation
+-------------
+
+As a bonus, it can start a player with initial inventory upon
+registration.  This is configured through the `nest-egg` setting.
+
+### Commands
+
+* *chpwd* _<old-pwd>_  
+  Used by players to change their passwords.
+* *resetpwd* _<player>_  
+  Used by ops to reset a players password.  This actually unregisters
+  the password.
+
+### Configuration
+
+	---
+	messages:
+	  re-enter pwd: 'Please re-enter password to confirm:'
+	  passwords dont match: |-
+	    Passwords do not match.
+	    Please try again!
+	    Enter a new password:
+	  register ok: You have been registered!
+	  no spaces: |-
+	    Password should not contain spaces
+	    or tabs
+	  not name: Password should not be your name
+	  too many login: You have attempted to login too many times.
+	  login timeout: Login timer expired!
+	nest-egg:
+	- "272:0:1"
+	- "17:0:16"
+	- "364:0:5"
+	- "266:0:10"
+	max-attempts: 5
+	login-timeout: 60
+	auto-ban: false
+	...
+
+* The section `messages` can be used to configure displayed texts.
+* `nest-egg` section contains list of items that will be given to the
+player upon registration.
+* `max-attempts` counts the number of tries to login.
+* `login-timeout` will kick the player out if not authenticated in
+  that number of seconds.
+* `auto-ban`: If set to true it will automatically ban the IP of a
+  player that does too many login attempts.
+
+Changes
+-------
+
+* 1.2.2: Auto-Ban
+  - Too many login attempts will cause the player to be banned.
+* 1.2.1: CallbackTask deprecation
+  * Removed CallbackTask deprecation warnings
+* 1.2.0: max-logins
+  * Suggestion from MCPEPIG
+    - kick user out after `max-attempts`.
+    - Added a chpwd command.
+  * Kick user out if not authenticated after `timeout` seconds.
+  * Added resetpwd command for ops
+* 1.1.0: Small update
+  * Added `nest-egg`
+  * Messages can be configured.
+* 1.0.0: First release
+
+Copyright
+---------
+
+    SimpleAuthHelper
+    Copyright (C) 2015 Alejandro Liu  
+    All Rights Reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ SimpleAuthHelper/src/aliuly/helper/Main.php
@@ -0,0 +1,288 @@
+<?php
+namespace aliuly\helper;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\item\Item;
+use pocketmine\utils\Config;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener,CommandExecutor {
+	protected $auth;
+	protected $pwds;
+	protected $chpwd;
+	protected $cfg;
+
+	public function onEnable(){
+		$this->auth = $this->getServer()->getPluginManager()->getPlugin("SimpleAuth");
+		if (!$this->auth) {
+			$this->getLogger()->info(TextFormat::RED."Unable to find SimpleAuth");
+			return;
+		}
+		if (!is_dir($this->getDataFolder())) mkdir($this->getDataFolder());
+		$defaults = [
+			"version" => $this->getDescription()->getVersion(),
+			"messages" => [
+				"re-enter pwd" => "Please re-enter password to confirm:",
+				"passwords dont match" => "Passwords do not match.\nPlease try again!\nEnter a new password:",
+				"register ok" => "You have been registered!",
+				"no spaces" => "Password should not contain spaces or tabs",
+				"not name" => "Password should not be your name",
+				"too many logins" => "You have attempted to login too many times.",
+				"login timeout" => "Login timer expired!",
+				"register first" => "You must first be registered",
+				"chpwd msg" => "Enter your new password:",
+				"chpwd error" => "Old password does not match",
+				"chpwd ok" => "Password changed succesfully",
+				"registration error" => "Registration error.  Try again later!",
+				"auth error" => "Authentication error.  Try again later!",
+			],
+			"nest-egg" => [
+				"STONE_SWORD:0:1",
+				"WOOD:0:16",
+				"COOKED_BEEF:0:5",
+				"GOLD_INGOT:0:10",
+			],
+			"max-attempts" => 5,
+			"login-timeout" => 60,
+			"auto-ban" => false,
+		];
+		if (file_exists($this->getDataFolder()."config.yml")) {
+			unset($defaults["nest-egg"]);
+		}
+		$this->cfg=(new Config($this->getDataFolder()."config.yml",
+										  Config::YAML,$defaults))->getAll();
+
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->pwds = [];
+	}
+	//////////////////////////////////////////////////////////////////////
+	//
+	// Event handlers
+	//
+	//////////////////////////////////////////////////////////////////////
+	public function onPlayerQuit(PlayerQuitEvent $ev) {
+		$n = $ev->getPlayer()->getName();
+		if (isset($this->pwds[$n])) unset($this->pwds[$n]);
+		if (isset($this->chpwd[$n])) unset($this->chpwd[$n]);
+	}
+	public function onPlayerJoin(PlayerJoinEvent $ev) {
+		if ($this->cfg["login-timeout"] == 0) return;
+		$n = $ev->getPlayer()->getName();
+		$this->getServer()->getScheduler()->scheduleDelayedTask(new PluginCallbackTask($this,[$this,"checkTimeout"],[$n]),$this->cfg["login-timeout"]*20);
+	}
+	/**
+	 * @priority LOW
+	 */
+	public function onPlayerCmd(PlayerCommandPreprocessEvent $ev) {
+		if ($ev->isCancelled()) return;
+		echo __METHOD__.",".__LINE__."\n"; //##DEBUG;
+		$pl = $ev->getPlayer();
+		$n = $pl->getName();
+		if ($this->auth->isPlayerAuthenticated($pl) && !isset($this->chpwd[$n])) return;
+
+		if (!$this->auth->isPlayerRegistered($pl) || isset($this->chpwd[$n])) {
+			if (!isset($this->pwds[$n])) {
+				if (!$this->checkPwd($pl,$ev->getMessage())) {
+					$ev->setCancelled();
+					$ev->setMessage("~");
+					return;
+				}
+				$this->pwds[$n] = $ev->getMessage();
+				$pl->sendMessage($this->cfg["messages"]["re-enter pwd"]);
+				$ev->setCancelled();
+				$ev->setMessage("~");
+				return;
+			}
+			if ($this->pwds[$n] != $ev->getMessage()) {
+				unset($this->pwds[$n]);
+				$ev->setCancelled();
+				$ev->setMessage("~");
+				$pl->sendMessage($this->cfg["messages"]["passwords dont match"]);
+				return;
+			}
+			if (isset($this->chpwd[$n])) {
+				// User is changing password...
+				unset($this->chpwd[$n]);
+				$ev->setMessage("~");
+				$ev->setCancelled();
+				$pw = $this->pwds[$n];
+				unset($this->pwds[$n]);
+
+				if (!$this->auth->unregisterPlayer($pl)) {
+					$pl->sendMessage($this->cfg["messages"]["registration error"]);
+					return;
+				}
+				if (!$this->auth->registerPlayer($pl,$pw)) {
+					$pl->kick($this->cfg["messages"]["registration error"]);
+					return;
+				}
+				$pl->sendMessage($this->cfg["messages"]["chpwd ok"]);
+				return;
+			}
+			// New user registration...
+			if (!$this->auth->registerPlayer($pl,$this->pwds[$n])) {
+				$pl->kick($this->cfg["messages"]["registration error"]);
+				return;
+			}
+			if (!$this->auth->authenticatePlayer($pl)) {
+				$pl->kick($this->cfg["messages"]["auth error"]);
+				return;
+			}
+			unset($this->pwds[$n]);
+			$ev->setMessage("~");
+			$ev->setCancelled();
+			$pl->sendMessage($this->cfg["messages"]["register ok"]);
+			if (isset($this->cfg["nest-egg"]) && !$pl->isCreative()) {
+				// Award a nest egg to player...
+				foreach ($this->cfg["nest-egg"] as $i) {
+					$r = explode(":",$i);
+					if (count($r) != 3) continue;
+					$item = Item::fromString($r[0].":".$r[1]);
+					$item->setCount(intval($r[2]));
+					$pl->getInventory()->addItem($item);
+				}
+			}
+			return;
+		}
+		$ev->setMessage("/login ".$ev->getMessage());
+		if ($this->cfg["max-attempts"] > 0) {
+			if (isset($this->pwds[$n])) {
+				++$this->pwds[$n];
+			} else {
+				$this->pwds[$n] = 1;
+			}
+			$this->getServer()->getScheduler()->scheduleDelayedTask(new PluginCallbackTask($this,[$this,"checkLoginCount"],[$n]),5);
+		}
+		return;
+	}
+	public function checkTimeout($n) {
+		echo __METHOD__.",".__LINE__."($n)\n"; //##DEBUG;
+		$pl = $this->getServer()->getPlayer($n);
+		if ($pl && !$this->auth->isPlayerAuthenticated($pl)) {
+			echo __METHOD__.",".__LINE__."($n)\n"; //##DEBUG;
+			$pl->kick($this->cfg["messages"]["login timeout"]);
+		}
+	}
+	public function checkLoginCount($n) {
+		echo __METHOD__.",".__LINE__."($n)\n"; //##DEBUG;
+		if (!isset($this->pwds[$n])) return;
+		echo __METHOD__.",".__LINE__."($n)\n"; //##DEBUG;
+		$pl = $this->getServer()->getPlayer($n);
+		if ($pl && !$this->auth->isPlayerAuthenticated($pl)) {
+			echo __METHOD__.",".__LINE__."($n)\n"; //##DEBUG;
+			if ($this->pwds[$n] >= $this->cfg["max-attempts"]) {
+				echo __METHOD__.",".__LINE__."($n)\n"; //##DEBUG;
+				if ($this->cfg["auto-ban"]) {
+					// OK banning use for trying to hack...
+					$ip = $pl->getAddress();
+					$this->getServer()->getIPBans()->addBan($ip,"Too many login attempts",null,"SimpleAuthHelper");
+					$this->getServer()->blockAddress($ip,-1);
+					$this->getServer()->broadcastMessage("[Helper] Banned IP Address $ip");
+				}
+
+				$pl->kick($this->cfg["messages"]["too many logins"]);
+				unset($this->pwds[$n]);
+			}
+			return;
+		}
+		unset($this->pwds[$n]);
+		return;
+	}
+	public function checkPwd($pl,$pwd) {
+		if (preg_match('/\s/',$pwd)) {
+			$pl->sendMessage($this->cfg["messages"]["no spaces"]);
+			return false;
+		}
+		if (strlen($pwd) < $this->auth->getConfig()->get("minPasswordLength")){
+			$pl->sendMessage($this->auth->getMessage("register.error.password"));
+			return false;
+		}
+		if (strtolower($pl->getName()) == strtolower($pwd)) {
+			$pl->sendMessage($this->cfg["messages"]["not name"]);
+			return false;
+		}
+		return true;
+	}
+	//////////////////////////////////////////////////////////////////////
+	//
+	// Commands
+	//
+	//////////////////////////////////////////////////////////////////////
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+		if (!$this->auth) {
+			$sender->sendMessage(TextFormat::RED."SimpleAuthHelper has been disabled");
+			$sender->sendMessage(TextFormat::RED."SimpleAuth not found!");
+			return true;
+		}
+		switch($cmd->getName()){
+			case "chpwd":
+				if (!($sender instanceof Player)) {
+					$sender->sendMessage(TextFormat::RED.
+												"This command only works in-game.");
+					return true;
+				}
+				if (count($args) == 0) return false;
+				if(!$this->auth->isPlayerRegistered($sender)) {
+					$sender->sendMessage($this->cfg["messages"]["register first"]);
+					return true;
+				}
+				$provider = $this->auth->getDataProvider();
+				if (($data = $provider->getPlayer($sender)) === null) {
+					$sender->sendMessage(TextFormat::RED.
+												"Internal Registration error");
+					return true;
+				}
+				$password = implode(" ", $args);
+				if(hash_equals($data["hash"], $this->hash(strtolower($sender->getName()), $password))) {
+					$this->chpwd[$sender->getName()] = $sender->getName();
+					$sender->sendMessage($this->cfg["messages"]["chpwd msg"]);
+					return true;
+				}else{
+					$sender->sendMessage($this->cfg["messages"]["chpwd error"]);
+					return false;
+				}
+				break;
+			case "resetpwd":
+				foreach($args as $name){
+					$player = $this->getServer()->getOfflinePlayer($name);
+					if($this->auth->unregisterPlayer($player)){
+						$sender->sendMessage(TextFormat::GREEN . "$name unregistered");
+						if($player instanceof Player){
+							$player->sendMessage(TextFormat::YELLOW."You are no longer registered!");
+							$this->auth->deauthenticatePlayer($player);
+						}
+					}else{
+						$sender->sendMessage(TextFormat::RED . "Unable to unregister $name");
+					}
+					return true;
+				}
+				break;
+		}
+		return false;
+	}
+	/**
+	 * COPIED FROM SimpleAuth by PocketMine team...
+	 *
+	 * Uses SHA-512 [http://en.wikipedia.org/wiki/SHA-2] and Whirlpool [http://en.wikipedia.org/wiki/Whirlpool_(cryptography)]
+	 *
+	 * Both of them have an output of 512 bits. Even if one of them is broken in the future, you have to break both of them
+	 * at the same time due to being hashed separately and then XORed to mix their results equally.
+	 *
+	 * @param string $salt
+	 * @param string $password
+	 *
+	 * @return string[128] hex 512-bit hash
+	 */
+	private function hash($salt, $password){
+		return bin2hex(hash("sha512", $password . $salt, true) ^ hash("whirlpool", $salt . $password, true));
+	}
+}
