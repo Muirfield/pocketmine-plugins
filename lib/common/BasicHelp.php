566e0087c011519a2a442225992377a9236cfc8d
Alejandro Liu <alejandro_liu@hotmail.com>
2015-05-27 01:30:21 +0200

    Added common code

--- lib/common/BasicHelp.php
+++ lib/common/BasicHelp.php
@@ -1,28 +1,34 @@
 <?php
-namespace aliuly\worldprotect;
+namespace aliuly\common;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\utils\TextFormat;
 use aliuly\common\mc;
+use aliuly\common\Cli;
+use aliuly\common\BasicCli;
 
-class WpHelp extends BaseWp {
-	public function __construct($owner) {
+class BasicHelp extends BasicCli {
+	protected $fmt;
+	public function __construct($owner,$fmt = "/%s %s %s") {
 		parent::__construct($owner);
 		$this->enableSCmd("help",["aliases"=>["?"]]);
+		$this->fmt = $fmt;
 	}
-	public function onSCommand(CommandSender $c,Command $cc,$scmd,$world,array $args) {
-		$cm = $this->owner->getScmdMap();
+	public function onSCommand(CommandSender $c,Command $cc,$scmd,$data,array $args) {
+		$cm = $this->owner->getSCmdMap();
 		$pageNumber = $this->getPageNumber($args);
 
 		if (count($args)) {
 			if ($args[0] == "usage") {
 				if (!isset($cm["usage"][$scmd])) return false;
-				$c->sendMessage(TextFormat::RED."Usage: /".$cc->getName().
-									 " [world] $scmd ".$cm["usage"][$scmd]);
+				$c->sendMessage(TextFormat::RED.mc::_("Usage: ").
+									 sprintf($this->fmt,
+												$cc->getName(),
+												$scmd,
+												$cm["usage"][$scmd]));
 				return true;
 			}
-
 			$txt = [ "Help for ".$cc->getName() ];
 
 			foreach ($args as $i) {
@@ -31,14 +37,15 @@ class WpHelp extends BaseWp {
 					$txt[] = TextFormat::RED.mc::_("No help for %1%",$i);
 					continue;
 				}
-				$txt[] = TextFormat::YELLOW.mc::_("Help: ").TextFormat::WHITE."/wp $i";
+				$txt[] = TextFormat::YELLOW.mc::_("Help: ").TextFormat::WHITE.
+						 "/".$cc->getName()." $i";
 				if (isset($cm["help"][$i]))
 					$txt[] = TextFormat::YELLOW.mc::_("Description: ").
 							 TextFormat::WHITE.$cm["help"][$i];
 				if (isset($cm["usage"][$i]))
 					$txt[] = TextFormat::YELLOW.mc::_("Usage: ").
-							 TextFormat::WHITE." /".$cc->getName().
-							 " [world] $i ".$cm["usage"][$i];
+							 TextFormat::WHITE.
+							 sprintf($this->fmt,$cc->getName(),$i,$cm["usage"][$i]);
 			}
 			return $this->paginateText($c,$pageNumber,$txt);
 		}
@@ -49,7 +56,7 @@ class WpHelp extends BaseWp {
 			foreach ($cm["alias"] as $i => $j) {
 				if ($j == $cn) $ln .= "|$i";
 			}
-			$ln .= ": ".TextFormat::WHITE."$desc";
+			$ln .= ": ".TextFormat::WHITE.$desc;
 			$txt[] = $ln;
 		}
 		return $this->paginateText($c,$pageNumber,$txt);
