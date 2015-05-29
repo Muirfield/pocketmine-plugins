9489d43ccf01b825ad521fef79a1cd4910541f5f
Alejandro Liu <alejandro_liu@hotmail.com>
2015-05-29 03:37:28 +0200

    Removed common library dependancy

--- NotSoFlat/src/aliuly/notsoflat/Main.php
+++ NotSoFlat/src/aliuly/notsoflat/Main.php
@@ -4,11 +4,11 @@ namespace aliuly\notsoflat;
 use pocketmine\plugin\PluginBase;
 use pocketmine\level\generator\Generator;
 use pocketmine\utils\TextFormat;
-use aliuly\common\MPMU;
 
 class Main extends PluginBase{
 	public function onEnable(){
-		if (MPMU::pmCheck($this->getServer(),"1.12.0")) {
+		$api = $this->getServer()->getApiVersion();
+		if (version_compare($api,"1.12.0") >= 0) {
 			$this->getLogger()->info(TextFormat::RED."WARNING: PMv1.5 support is experimental!");
 			Generator::addGenerator(NotSoFlat::class, "notsoflat");
 		} else {
