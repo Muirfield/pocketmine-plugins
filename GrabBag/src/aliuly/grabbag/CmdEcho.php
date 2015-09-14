d9eae34876baa795059b7ba35277c20dd5600e92
Alejandro Liu <alejandro_liu@hotmail.com>
2015-09-14 13:45:10 +0200

    WIP

---
+++ GrabBag/src/aliuly/grabbag/CmdEcho.php
@@ -0,0 +1,35 @@
+<?php
+//= cmd:echo,Developer_Tools
+//: shows the given text
+//>  usage: **echo** _[text]_
+//:
+namespace aliuly\loader;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+use aliuly\common\mc;
+use aliuly\common\BasicCli;
+use aliuly\common\PermUtils;
+
+class EchoCmd extends BasicCli implements CommandExecutor {
+	public function __construct($owner) {
+		parent::__construct($owner);
+    PermUtils::add($this->owner, "gb.cmd.echo", "echo command", "true");
+    PermUtils::add($this->owner, "gb.cmd.rem", "remark command", "true");
+    $this->enableCmd("echo",
+							  ["description" => mc::_("display text"),
+								"usage" => mc::_("/echo [text]"),
+								"permission" => "gb.cmd.echo"]);
+    $this->enableCmd("rem",
+							  ["description" => mc::_("don't display text"),
+								"usage" => mc::_("/rem [text]"),
+								"permission" => "gb.cmd.rem"]);
+	}
+  public function onCommand(CommandSender $sender,Command $cmd,$label, array $args) {
+    if (strtolower($cmd->getName()) == "rem") return true;
+    $cmdline = implode(" ",$args);
+    $sender->sendMessage($cmdline);
+    return true;
+	}
+}
