e09c32ecac70a1a67db524d1056a5e0bcc1dadfb
alejandroliu <alejandro_liu@hotmail.com>
2016-10-31 14:31:53 +0100

    Killing it

--- LiveSigns/src/aliuly/livesigns/fetcher/SignFetcher.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace aliuly\livesigns\fetcher;
-
-abstract class SignFetcher {
-	static public function fetch($dat,$cfg) {
-		return ["","","",""];
-	}
-	static public function default_age() { return -1; }
-}
--- LiveSigns/src/aliuly/livesigns/fetcher/Text.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace aliuly\livesigns\fetcher;
-
-abstract class Text extends SignFetcher {
-	static public function fetch($dat,$cfg) {
-		if (!is_array($dat["content"])) return [$dat["content"]];
-		return $dat["content"];
-	}
-}
